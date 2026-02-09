375c194409bea95d9dbbd03590d3439ff56c03ca
James <162901067+Enjimneering@users.noreply.github.com>
2026-02-09 01:05:35 +0000

    add starting point for register file

---
+++ hw/rtl/reg_file.v
@@ -0,0 +1,15 @@
+// =======================================================================
+// Module:      RV32I Register File
+// Project:     Ripple-32
+// Description: The register file contains 32 registers, each 32 bits wide. 
+// =======================================================================
+
+module reg_file (
+    input wire        clk,        
+    input wire        rst_n
+    // TODO add i/o ports for register file read/write
+    
+  );
+
+
+endmodule
