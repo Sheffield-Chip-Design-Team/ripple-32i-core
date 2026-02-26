1d8471012ba65670bcf21674ee7e89e0e56775e7
Swapnanil-06 <swapnanilchattopadhyay06@gmail.com>
2026-02-26 22:17:26 +0000

    add store opcode

--- hw/rtl/opcode_decoder.v
+++ hw/rtl/opcode_decoder.v
@@ -31,11 +31,13 @@ module opcode_decoder (
   localparam [6:0] ALU_IMM_OPCODE = 7'b0010011;
   localparam [6:0] BRANCH_OPCODE  = 7'b1100011;
   localparam [6:0] LOAD_OPCODE    = 7'b0000011;
- 
+  localparam [6:0] STORE_OPCODE   = 7'b0100011;
+
   assign isALUreg = (opcode[6:0] == ALU_REG_OPCODE);   // rd <- rs1 OP rs2   
   assign isALUimm = (opcode[6:0] == ALU_IMM_OPCODE);   // rd <- rs1 OP Iimm
   assign isBranch = (opcode[6:0] == BRANCH_OPCODE);
   assign isLoad   = (opcode[6:0] == LOAD_OPCODE);      // if(rs1 OP rs2) PC<-PC+Bimm
+  assign isStore  = (opcode[6:0] == STORE_OPCODE);
   // ... add more instruction types here
 
 endmodule
