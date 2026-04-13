ea69085f2f83474666c7c32ba77227f192d815c4
trisaya1 <tvillasaya06@gmail.com>
2026-04-13 09:16:39 +0100

    enabling control unit testing

--- hw/tb/control_unit/control_unit_wtb.v
+++ hw/tb/control_unit/control_unit_wtb.v
@@ -11,13 +11,39 @@ module control_unit_wtb;
   wire [4:0] rs1;
   wire [4:0] rs2;
   wire [4:0] rd;
+  wire [3:0] alu_control;
+  wire [1:0] alu_a_sel;
+  wire  alu_b_sel;
+  wire  reg_write;
+  wire  mem_read;
+  wire  mem_write;
+  wire [1:0] mem_size;
+  wire  load_unsigned;
+  wire [1:0] wb_sel;
+  wire [2:0] branch_type;
+  wire  jump;
+  wire  jalr;
+  wire  illegal_instr;
 
 control_unit dut (
       .instr(instr),
       .imm(imm),
       .rs1(rs1),
       .rs2(rs2),
-      .rd(rd)
+      .rd(rd),
+      .alu_control(alu_control),
+      .alu_a_sel(alu_a_sel),
+      .alu_b_sel(alu_b_sel),
+      .reg_write(reg_write),
+      .mem_read(mem_read),
+      .mem_write(mem_write),
+      .mem_size(mem_size),
+      .load_unsigned(load_unsigned),
+      .wb_sel(wb_sel),
+      .branch_type(branch_type),
+      .jump(jump),
+      .jalr(jalr),
+      .illegal_instr(illegal_instr)
   );
 
 endmodule 
